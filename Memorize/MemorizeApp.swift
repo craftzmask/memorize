348eedd2c8ae159662d08f571f8de882578de0d3
Khanh Chung <khanh.d.chung@gmail.com>
2023-10-21 23:17:04 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Khanh Chung on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
