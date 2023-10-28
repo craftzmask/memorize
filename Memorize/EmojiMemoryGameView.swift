d34f08c2ddf7f3ff4bff84644c6b51cc4bc984b3
Khanh Chung <khanh.d.chung@gmail.com>
2023-10-27 21:40:12 -0700

    add animation to make cards fly around when shuffle

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -28,9 +28,10 @@ struct EmojiMemoryGameView: View {
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards.indices, id: \.self) { index in
-                CardView(card: viewModel.cards[index])
+            ForEach(viewModel.cards) { card in
+                CardView(card: card)
                     .aspectRatio(2 / 3, contentMode: .fit)
+                    .animation(.default, value: viewModel.cards)
             }
         }
         .foregroundColor(.orange)
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -7,15 +7,15 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: [Card]
     
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
         cards = []
         for pairIndex in 0..<numberOfPairsOfCards {
             let content = cardContentFactory(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(content: content, id: "\(pairIndex + 1)a"))
+            cards.append(Card(content: content, id: "\(pairIndex + 1)b"))
         }
     }
     
@@ -27,9 +27,11 @@ struct MemoryGame<CardContent> {
         print(card)
     }
     
-    struct Card {
+    struct Card: Equatable, Identifiable {
         var isFaceUp = true
         var isMatched = false
         let content: CardContent
+        
+        let id: String
     }
 }
